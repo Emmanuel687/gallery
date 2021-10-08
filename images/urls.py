93e40dbd1a696323267e9dec5cdea987c454bf4e
shalin <shalin.rono@student.moringaschool.com>
2021-10-08 22:00:55 +0300

    create template files

--- images/urls.py
+++ images/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     # path('home/',getHomePage),
     path('images/',views.images,name="images"),
