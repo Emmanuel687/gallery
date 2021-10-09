b2cad546bf2e465c070f4a94ec85e63a2c6e614f
shalin <shalin.rono@student.moringaschool.com>
2021-10-09 11:14:35 +0300

    define image functions

--- images/urls.py
+++ images/urls.py
@@ -2,12 +2,19 @@ from django.conf.urls import url
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+
 
 urlpatterns = [
+    # url('^$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search, name='search_results'),
+    # url(r'^image/(\d+)',views.image,name ='image'),
+    # path('category/<str:category_name>',views.image, name="image"),
+    # url(r'^location',views.location,name='location'),
     # path('home/',getHomePage),
     url(r'^$',views.welcome,name='index'),
-    url(r'^search/',views.search,name='search_results'),
-    url(r'^images/(\d+)',views.image,name='image'),
+    # url(r'^search/',views.search,name='search_results'),
+    # url(r'^images/(\d+)',views.image,name='image'),
     url(r'^location/',views.location,name='location'),
 ]
 if settings.DEBUG:
--- images/views.py
+++ images/views.py
@@ -16,4 +16,29 @@ def search(request):
         searched_images = Image.search_image(search_term)
         message = f"{search_term}"
 
+        return render(request,"search.html",{"message":message,"images":searched_images})
+
+    else:
+        message = "You haven't searched for any category"
+        return render(request,"search.html",{"message":message})
+
+def image(request,image_id):
+    try:
+        image = Image.get_image_by_id(image_id)
+
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,"image.html",{"image":image})
+
+def location(request):
+    if 'location' in request.GET and request.GET["location"]:
+        location = request.GET.get("location")
+        searched_images = Image.filter_by_location(location)
+        message = f"{location}"
+        return render(request,"search.html",{"message":message,"images":searched_images})
+
+    else:
+        message = "select location to filter"
+        return render(request,"search.html",{"message":message})
+
 
