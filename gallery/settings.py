bdaf1729cf43f10820a60e261d82aec51e7cba25
shalin <shalin.rono@student.moringaschool.com>
2021-10-09 09:23:45 +0300

    define function for the index page

--- gallery/settings.py
+++ gallery/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'images.apps.ImagesConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
--- images/views.py
+++ images/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Location,Category,Image
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
-from django.http import HttpResponse
 
-def images(request):
-    return HttpResponse("View the images here")
+def welcome(request):
+    images = Image.all_images()
+    return render (request,'index.html',{"images":images})
 
 
