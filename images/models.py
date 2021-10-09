1a839ea0daa0277b1ef23d5492cb32161c7f510a
shalin <shalin.rono@student.moringaschool.com>
2021-10-09 09:27:19 +0300

    create model class location

--- images/models.py
+++ images/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
