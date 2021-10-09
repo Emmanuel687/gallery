7c2cee09164fa57ea70c8d01c2934badc9816622
shalin <shalin.rono@student.moringaschool.com>
2021-10-09 10:15:38 +0300

    create image test class

--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Image, Category, Location
 
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.location = Location(name="Thika")
+        self.location.save()
+
+        self.category = Category(name="Trip")
+        self.category.save()
